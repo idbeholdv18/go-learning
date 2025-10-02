9dd44e4f4c9647cd482e249af0bdb3194ecfc2bc
idbeholdv18 <alimsadullaev18@gmail.com>
2025-10-03 00:39:01 +0300

    dup v2 with file reading

---
+++ ch1/dup2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func count_lines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
+func main() {
+	counts := make(map[string]int)
+
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		count_lines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			count_lines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
